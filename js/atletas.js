fca9f5e473ff5aa2f3432b358e29def3506fb820
Bernardo <bemascarenhas6@gmail.com>
2023-11-09 23:38:45 -0300

    commit final de hoje 11/9

--- js/atletas.js
+++ js/atletas.js
@@ -1,46 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const botaofeminino = document.getElementById("exibirFeminino");
-    const botaomasculino = document.getElementById("exibirMasculino");
-    const botaotodos = document.getElementById("exibirtodos");
+    const buttons = document.querySelectorAll(".filtro-btn");
 
-    // Adicione manipuladores de evento aos botões
-    botaofeminino.addEventListener("click", () => gerarseleção("feminino"));
-    botaomasculino.addEventListener("click", () => gerarseleção("masculino"));
-    botaotodos.addEventListener("click", () => gerarseleção("all"));
+    buttons.forEach(button => {
+        button.addEventListener("click", () => gerarSelecao(button.dataset.gender));
+    });
 
-    // Função para carregar o elenco com base no gênero
-    function gerarseleção(genero) {
-        const seleçãocontainer = document.getElementById("elenco");
-        seleçãocontainer.innerHTML = ''; // Limpar conteúdo
+    function gerarSelecao(genero) {
+        const selecaoContainer = document.getElementById("elenco");
+        selecaoContainer.innerHTML = ''; // Limpar conteúdo
 
-        // Fazer uma solicitação para a API externa com base no gênero
         fetch(`https://botafogo-atletas.mange.li/${genero}`)
             .then(response => response.json())
             .then(data => {
                 data.forEach(jogador => {
-                    const figurinhajogador = document.createElement('div');
-                    figurinhajogador.className = 'atleta-card';
+                    const figurinhaJogador = document.createElement('div');
+                    figurinhaJogador.className = 'atleta-card';
 
-                    const fotojogador = document.createElement('img');
-                    fotojogador.className = 'atleta-img';
-                    fotojogador.src = jogador.imagem;
-
-                    // Adiciona evento de clique na imagem do atleta para redirecionar
-                    fotojogador.addEventListener('click', function() {
-                        // Redirecionar para a página do atleta com o ID na query string
+                    const fotoJogador = document.createElement('img');
+                    fotoJogador.className = 'atleta-img';
+                    fotoJogador.src = jogador.imagem;
+                    fotoJogador.addEventListener('click', () => {
                         window.location.href = `atletas_totais.html?id=${jogador.id}`;
                     });
 
                     const detalhesAtleta = document.createElement('div');
                     detalhesAtleta.className = 'atleta-detalhes';
-                    detalhesAtleta.innerHTML = `
-                        <p>Clique na imagem e veja mais!</p>
-                    `;
+                    detalhesAtleta.innerHTML = `<p>Clique na imagem e veja mais!</p>`;
 
-                    figurinhajogador.appendChild(fotojogador);
-                    figurinhajogador.appendChild(detalhesAtleta);
+                    figurinhaJogador.appendChild(fotoJogador);
+                    figurinhaJogador.appendChild(detalhesAtleta);
 
-                    seleçãocontainer.appendChild(figurinhajogador);
+                    selecaoContainer.appendChild(figurinhaJogador);
                 });
             })
             .catch(error => {
