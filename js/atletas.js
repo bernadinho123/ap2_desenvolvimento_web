56a35f953d1fe4e69613874bad60ae564f123761
Bernardo <bemascarenhas6@gmail.com>
2023-11-15 19:14:49 -0300

    quinta-feira-commit

--- js/atletas.js
+++ js/atletas.js
@@ -1,92 +1,75 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".filtro-btn");
+function gerarSelecao(genero) {
+    const selecaoContainer = document.getElementById("elenco");
+    selecaoContainer.innerHTML = ''; // Limpar conteúdo
 
-    buttons.forEach(button => {
-        button.addEventListener("click", () => gerarSelecao(button.dataset.gender));
-    });
+    const filtroSelecao = document.getElementById('filtro-selecao');
+    const generoSelecionado = genero || filtroSelecao.value;
 
-    function gerarSelecao(genero) {
-        const selecaoContainer = document.getElementById("elenco");
-        selecaoContainer.innerHTML = ''; // Limpar conteúdo
+    fetch(`https://botafogo-atletas.mange.li/${generoSelecionado}`)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(jogador => {
+                const figurinhaJogador = document.createElement('div');
+                figurinhaJogador.className = 'atleta-card';
 
-        fetch(`https://botafogo-atletas.mange.li/${genero}`)
-            .then(response => response.json())
-            .then(data => {
-                data.forEach(jogador => {
-                    const figurinhaJogador = document.createElement('div');
-                    figurinhaJogador.className = 'atleta-card';
-
-                    const fotoJogador = document.createElement('img');
-                    fotoJogador.className = 'atleta-img';
-                    fotoJogador.src = jogador.imagem;
-                    fotoJogador.addEventListener('click', () => {
-                        window.location.href = `atletas_totais.html?id=${jogador.id}`;
-                    });
-
-                    const detalhesAtleta = document.createElement('div');
-                    detalhesAtleta.className = 'atleta-detalhes';
-                    detalhesAtleta.innerHTML = `<p>Clique na imagem e veja mais!</p>`;
-
-                    figurinhaJogador.appendChild(fotoJogador);
-                    figurinhaJogador.appendChild(detalhesAtleta);
-
-                    selecaoContainer.appendChild(figurinhaJogador);
+                const fotoJogador = document.createElement('img');
+                fotoJogador.className = 'atleta-img';
+                fotoJogador.src = jogador.imagem;
+                fotoJogador.addEventListener('click', () => {
+                    window.location.href = `atletas_totais.html?id=${jogador.id}`;
                 });
-            })
-            .catch(error => {
-                console.error(`Erro ao buscar elenco ${genero}:`, error);
+
+                const detalhesAtleta = document.createElement('div');
+                detalhesAtleta.className = 'atleta-detalhes';
+                detalhesAtleta.innerHTML = `<p>Clique na imagem e veja mais!</p>`;
+
+                figurinhaJogador.appendChild(fotoJogador);
+                figurinhaJogador.appendChild(detalhesAtleta);
+
+                selecaoContainer.appendChild(figurinhaJogador);
             });
-    }
-});
-document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".filtro-btn");
-    buttons.forEach(button => {
-        button.addEventListener("click", () => gerarSelecao(button.dataset.gender));
-    });
 
-    function gerarSelecao(genero) {
-        const selecaoContainer = document.getElementById("elenco");
-        selecaoContainer.innerHTML = ''; // Limpar conteúdo
-
-        if (genero === 'todos') {
-            fetchData('feminino');
-            fetchData('masculino');
-        } else {
-            fetchData(genero);
-        }
-    }
-
-    function fetchData(genero) {
-        fetch(`https://botafogo-atletas.mange.li/${genero}`)
-            .then(response => response.json())
-            .then(data => {
-                data.forEach(jogador => {
-                    criarCartaoAtleta(jogador);
-                });
-            })
-            .catch(error => {
-                console.error(`Erro ao buscar elenco ${genero}:`, error);
-            });
-    }
-
-    function criarCartaoAtleta(jogador) {
-        const figurinhaJogador = document.createElement('div');
-        figurinhaJogador.className = 'atleta-card';
-
-        const fotoJogador = document.createElement('img');
-        fotoJogador.className = 'atleta-img';
-        fotoJogador.src = jogador.imagem;
-        fotoJogador.addEventListener('click', () => {
-            window.location.href = `atletas_totais.html?id=${jogador.id}`;
+            // Mostrar o conteúdo após buscar e criar os elementos
+            selecaoContainer.style.display = 'block';
+        })
+        .catch(error => {
+            console.error(`Erro ao buscar elenco ${generoSelecionado}:`, error);
         });
+}
 
-        const detalhesAtleta = document.createElement('div');
-        detalhesAtleta.className = 'atleta-detalhes';
-        detalhesAtleta.innerHTML = `<p>Clique na imagem e veja mais!</p>`;
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".filtro-btn");
+    const filtroSelecao = document.getElementById('filtro-selecao');
 
-        figurinhaJogador.appendChild(fotoJogador);
-        figurinhaJogador.appendChild(detalhesAtleta);
+    buttons.forEach(button => {
+        button.addEventListener("click", () => gerarSelecao(button.dataset.gender));
+    });
 
-        document.getElementById("elenco").appendChild(figurinhaJogador);
-    }
+    filtroSelecao.addEventListener('change', () => gerarSelecao());
+
+    // Inicialmente, ocultar o conteúdo
+    const selecaoContainer = document.getElementById("elenco");
+    selecaoContainer.style.display = 'none';
+
+    // Lógica para alternar entre os botões e o botão de seleção
+    window.addEventListener('resize', function() {
+        var larguraTela = window.innerWidth;
+
+        if (larguraTela <= 768) {
+            // Se a tela for menor ou igual a 768px, mostrar o botão de seleção e ocultar os botões
+            buttons.forEach(button => {
+                button.style.display = 'none';
+            });
+            filtroSelecao.style.display = 'inline-block';
+        } else {
+            // Se a tela for maior que 768px, mostrar os botões e ocultar o botão de seleção
+            buttons.forEach(button => {
+                button.style.display = 'inline-block';
+            });
+            filtroSelecao.style.display = 'none';
+        }
+    });
+
+    // Disparar o evento de redimensionamento ao carregar a página para definir inicialmente a exibição correta
+    window.dispatchEvent(new Event('resize'));
 });
